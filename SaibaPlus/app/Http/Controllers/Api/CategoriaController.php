0d34d0e152de4f3b14e954c97cd8bad11aa9f879
unknown <matheusnascimentoadegas@alu.ufc.br>
2026-07-03 17:12:31 -0300

    Commit do git bash pro github

---
+++ SaibaPlus/app/Http/Controllers/Api/CategoriaController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Categoria;
+
+class CategoriaController extends Controller
+{
+    public function index()
+    {
+        $categorias = Categoria::orderBy('nome')->get();
+
+        return response()->json([
+            'success' => true,
+            'data'    => $categorias,
+        ]);
+    }
+}
